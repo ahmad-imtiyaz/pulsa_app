26b30535cbfaf722d52562affb49277ffa1d1dc9
Prince Yazna <imtiyaznajih8@gmail.com>
2026-09-09 12:11:41 +0700

    Change app name from 'Pulsa App' to 'Bakul Pulsa'

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Pulsa App') }}</title>
+        <title>{{ config('app.name', 'Bakul Pulsa') }}</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -23,7 +23,7 @@
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                         </svg>
                     </div>
-                    <span class="text-2xl font-bold text-gray-900">{{ config('app.name', 'Pulsa App') }}</span>
+                    <span class="text-2xl font-bold text-gray-900">{{ config('app.name', 'Bakul Pulsa') }}</span>
                 </a>
             </div>
 
